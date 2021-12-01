09382024f6cdc8f48bbbf1ca82f60a412a72e887
dieudonne100 <61232003+dieudonne100@users.noreply.github.com>
2021-12-01 22:27:18 +0100

    Update infomation.php

---
+++ infomation.php
@@ -0,0 +1,183 @@
+<?php
+  
+    $profiler_info = array(
+        "userName" => "LIBAWO DIEUDONNE",
+        "profession" => "Développeur Web/Mobile",
+        "Date_naiss" => "Née le 29 Septembre 1999",
+        "Origine" => "Originaire de l'Ouest Cameroun",
+        "Status" => "Célibataire ",
+        "RasStatus" => " RAS",
+        "quatiere" => "Logbessou",
+        "ville" => "Douala",
+        "pays" => "Cameroun",
+        "Map1" => "4.6783289",
+        "Map2" => "44874874",
+        "tel" => "(237) 671758237",
+        "telDesc" => "Mobile, Telegram, Whatsapp",
+        "email" => "libawod @gmail.com",
+        "emailDesc" => "Google+, Twitter, LinkedIn, Github",
+        "email" => "libawod @gmail.com",
+        "projectCount" => "45",
+        "contractCount" => "31",
+        "expCount" => "12",
+        "langue1"=>"Francais",
+        "langue2"=>"Anglais",
+    );
+
+    $profiler_skills = array(
+        "skill1" => array
+        (
+            'title' => 'Developpement front-end',
+            'desc' => 'HTML5, SASS, VueJS, Angular, JavaFx,...',
+            'level' => 90,
+        ),
+        "skill2" => array
+        (
+            'title' => 'Developpement back-end',
+            'desc' => 'NodeJS, Drupal 8, Laravel, Kotlin, Java EE 7',
+            'level' => 70,
+        ),
+        "skill3" => array
+        (
+            'title' => 'Developpement mobile',
+            'desc' => 'Android Kotlin, IOS Swift, Cordova, Flutter',
+            'level' => 80,
+        ),
+        "skill4" => array
+        (
+            'title' => 'UI / UX Design',
+            'desc' => 'Photoshop CC, Adobe XD, Material Design',
+            'level' => 65,
+        ),
+        "skill5" => array
+        (
+            'title' => 'Base de donnees & big Data',
+            'desc' => 'Oracle 11g, PostgreSQL, Hadoop, Talend DI',
+            'level' => 95,
+        ),
+        "skill6" => array
+        (
+            'title' => 'Outils / Environments',
+            'desc' => 'Visual Paradigm, Git, Docker, K8s, Linux',
+            'level' => 85,
+        ),
+
+    );
+
+    $profiler_exps = array(
+        "experience1" => array
+        (
+            "jobTitle" => "Chef des projets technologique",
+            "companyName" => "@Ets. M DE M",
+            "startDate" => "Juillet 2019",
+            "endDate" => "ce jour",
+            "companySite" => "http://mdem.com",
+            "jobDesc" => "Chef du projet annuaire-universel.cm de l'ART",
+        ),
+        "experience2" => array
+        (
+            "jobTitle" => "Fondateur & DT",
+            "companyName" => "@Startup ChickDev",
+            "startDate" => "Juin 2015",
+            "endDate" => "ce jour",
+            "companySite" => "http://chickdev.com",
+            "jobDesc" => "Realisaition de plusieurs sites web et application web et mobiles",
+        ),
+        "experience3" => array
+        (
+            "jobTitle" => "Enseignant",
+            "companyName" => "@Institut Universitaire de la Cote",
+            "startDate" => "Octobre 2011",
+            "endDate" => "ce jour",
+            "companySite" => "http://myiuc.com",
+            "jobDesc" => '"Analyse. UML & MERISE"; "BD/SQL, ORACLE & MySQL";
+                            "Dev. IOS & Android"; "BI & Big Data Talend DI & Hadoop". 1<sup>ere</sup>, 2<sup>eme</sup>, 4<sup>eme</sup> annee',
+        ),
+        "experience4" => array
+        (
+            "jobTitle" => "Developpeur en chef",
+            "companyName" => "@Kayroual group",
+            "startDate" => "Mai 2013",
+            "endDate" => "Juin 2015",
+            "companySite" => "http://kayroual.com",
+            "jobDesc" => "Realisation de multiple projets logiciels et web, inforgraphie, ...",
+        ),
+        "experience5" => array
+        (
+            "jobTitle" => "Responsable commercial",
+            "companyName" => "@BAO Sarl",
+            "startDate" => "Decembre 2012",
+            "endDate" => "Juin 2013",
+            "companySite" => "http://bao-sarl.com",
+            "jobDesc" => "Definition des strategies commerciales,Controle de qualite, suivi...",
+        ),
+    );
+
+    $profiler_academic = array(
+        "academique1" => array
+        (
+            "diploma" => "DIPET 2 Electrotechnique",
+            "school" => "ENSET de Douala",
+            "time" => "Aout 2016",
+            "desc" => "Gestion d'eclairage d'une maison (Android + Adruino)",
+        ),
+        "academique2" => array
+        (
+            "diploma" => "Oracle Certified Associate",
+            "school" => "Kentnix Sarl",
+            "time" => "Mars 2009",
+            "desc" => "Oracle database 11g Administration",
+        ),
+        "academique3" => array
+        (
+            "diploma" => "Oracle SQl Certified",
+            "school" => "Kentnix Sarl",
+            "time" => "Decembre 2008",
+            "desc" => "SQl 2, SQL 3, XML",
+        ),
+        "academique4" => array
+        (
+            "diploma" => "Licence professionnelle",
+            "school" => "Douala Institute of Tech.",
+            "time" => "Octobre 2008",
+            "desc" => "Telecommunication & Reseaux",
+        ),
+        "academique5" => array
+        (
+            "diploma" => "DEC / BTS",
+            "school" => "CCNB Dieppe - Canada",
+            "time" => "Septembre 2007",
+            "desc" => "Programmation Appliquee Pour Internet",
+        ),
+        "academique6" => array
+        (
+            "diploma" => "Baccalaureat",
+            "school" => "Lycee Technique de Douala Bassa",
+            "time" => "Aout 2005",
+            "desc" => "Electrotechnique, mention BIEN (major de centre)",
+        ),
+    );
+        $entete=array(
+                "entetefirst"=>array(
+                "Exppr"=>"Experience professionnelle",
+                "Curac"=>"Cursus academique",
+                "Pointint"=>"Point d'interets",
+                "Langues"=>"Langues"
+            ),
+                "entetesecond"=>array(
+                "Expertise"=>"Expertise en entreprise",
+                "Diplomesfor"=>"Diplomes et formations certifiantes",
+                "Passetemps"=>"Passe-temps pour se faire un peu plaisir",
+                "Pratiqueentre"=>"Pratiquees en entreprise",
+            )
+                
+            );
+        
+            $about_recap=array(
+                "CONTRATS"=>"CONTRATS",
+                "PROJETS"=>"PROJETS",
+                "ANSEXP"=>"ANS D'EXP",
+
+            )
+   
+?>
